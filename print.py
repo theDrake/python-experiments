42edf394d4be9abe6aacf829a4db8d3e99386f96
David C. Drake <dcdrake42@gmail.com>
2015-03-08 19:54:16 -0600

    Add "print.py"

---
+++ print.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+print '\'hello\''
+print '"hello"'
+print "\"hello\""
+print "'hello'"
+print """In this string, 'I' can use "both" types without a '\\'!"""
+print '''Same 'with' "this" string!'''
