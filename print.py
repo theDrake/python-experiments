e1954697b9e35e8ea73f8cfd92026982174bbedb
theDrake <dcdrake42@gmail.com>
2015-07-28 20:12:58 -0600

    Update print.py

--- print.py
+++ print.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
-print '\'hello\''
-print '"hello"'
-print "\"hello\""
-print "'hello'"
-print """In this string, 'I' can use "both" types without a '\\'!"""
-print '''Same 'with' "this" string!'''
+# Single quotes and double quotes:
+print('\'hello\'')
+print('"hello"')
+print("\"hello\"")
+print("'hello'")
+print("""In this string, I can use ' and " without a '\\'!""")
+print('''Same 'with' "this" string!''')
+
+# Variables:
+balance = 1446.509
+print(balance)
+print(str(balance))
+print('Remaining balance: $%.2f' % balance)
+name = input('What is your name? ')
+print(name)
+print(name.upper())
+print(name.lower())
+print('Your name is ' + str(len(name)) + ' letters long.')
+description = 'awesome'
+print('You are totally %s!!!' % description)
