1f71153cf814f7d34835cea6eafe44683035d874
theDrake <dcdrake42@gmail.com>
2015-08-07 02:45:45 -0600

    Add compare_files.py

---
+++ compare_files.py
@@ -0,0 +1,18 @@
+import difflib
+
+def compare_files(filename1, filename2):
+    f = open(filename1, "r")
+    filelines1 = f.readlines()
+    f.close()
+    f = open(filename2, "r")
+    filelines2 = f.readlines()
+    f.close()
+    diffs = difflib.context_diff(filelines1,
+                                 filelines2,
+                                 fromfile=filename1,
+                                 tofile=filename2)
+    count = 0
+    for line in diffs:
+        print line,
+        count += 1
+    return count == 0
